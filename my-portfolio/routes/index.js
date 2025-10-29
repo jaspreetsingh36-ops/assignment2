07a19554fcfa6dc7ff5db91071fc633c9f7c7573
jaspreetsingh36-ops <jaspreet.singh36@ontariotechu.net>
2025-10-29 00:31:23 -0400

    Please enter the commit message for your changes. Lines starting

---
+++ my-portfolio/routes/index.js
@@ -0,0 +1,24 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home - Your Name' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me - Your Name' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects - Your Name' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact - Your Name' });
+});
+
+module.exports = router;
