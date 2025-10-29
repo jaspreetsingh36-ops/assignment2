74803ffd9799971a057a76f724290673994ca69a
jaspreetsingh36-ops <jaspreet.singh36@ontariotechu.net>
2025-10-29 01:46:08 -0400

    modified:   my-portfolio/views/projects.ejs

--- my-portfolio/routes/index.js
+++ my-portfolio/routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home - Your Name' });
+  res.render('index', { title: 'Home - Jaspreet Singh' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me - Your Name' });
+  res.render('about', { title: 'About Me - Jaspreet Singh' });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects - Your Name' });
+  res.render('projects', { title: 'Projects - Jaspreet Singh' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact - Your Name' });
+  res.render('contact', { title: 'Contact - Jaspreet Singh' });
 });
 
 module.exports = router;
