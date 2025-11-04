747b25b6a53f34ed3601e9f29ed6ce54556a41ee
jaspreetsingh36-ops <jaspreet.singh36@ontariotechu.net>
2025-11-03 21:22:59 -0500

    update index.js

---
+++ my-portfolio/routes/index.js
@@ -0,0 +1,24 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home - Jaspreet Singh' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me - Jaspreet Singh' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects - Jaspreet Singh' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact - Jaspreet Singh' });
+});
+
+module.exports = router;
